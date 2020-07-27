c8b41c75e308694b331f0ba92782751c88e94107
Zoë Knox <zoe@pixin.net>
2020-07-27 17:40:37 -0400

    Allow clearing current blocks. Add item frame selection.

--- src/main/java/com/opennms/minecraftnoc/ClickListener.java
+++ src/main/java/com/opennms/minecraftnoc/ClickListener.java
@@ -6,11 +6,14 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
 
 import java.util.logging.Level;
 
@@ -22,25 +25,58 @@ public class ClickListener implements Listener {
     }
 
     @EventHandler
-    public void onBlockClick(PlayerInteractEvent e) {
-        if(e.getHand() != EquipmentSlot.HAND || e.getAction() != Action.LEFT_CLICK_BLOCK) {
+    public void onEntityInteract(PlayerInteractEvent e) {
+        if(e.getAction() == Action.LEFT_CLICK_AIR) {
+            plugin.clearCurrentBlock();
+            plugin.clearCurrentEntity();
+            Player p = e.getPlayer();
+            p.sendMessage(ChatHelper.format("Cleared current item and block"));
+        }
+    }
+
+    @EventHandler
+    public void onEntityInteract(EntityDamageByEntityEvent e) {
+        if(e.getEntityType() != EntityType.ITEM_FRAME) {
             return;
         }
 
-        if(e.getPlayer().getInventory().getItemInMainHand().getType() != Material.STICK) {
+        Player p = (Player)e.getDamager();
+        if(p.getInventory().getItemInMainHand().getType() != Material.STICK) {
             return;
         }
 
-        Block b = e.getClickedBlock();
-        if(!(b.getState() instanceof Sign)) {
-            return;
-        }
-
-        Location loc = b.getLocation();
+        Entity ent = e.getEntity();
+        Location loc = ent.getLocation();
         int X = loc.getBlockX();
         int Y = loc.getBlockY();
         int Z = loc.getBlockZ();
-        Bukkit.getLogger().log(Level.WARNING, "Clicked block at "+X+","+Y+","+Z);
+        Bukkit.getLogger().log(Level.WARNING, "Clicked entity at " + X + "," + Y + "," + Z);
+
+        plugin.setCurrentEntity(ent);
+        p.sendMessage(ChatHelper.format("Current item set!"));
+
         e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onBlockClick(BlockBreakEvent e) {
+        Player p = e.getPlayer();
+        if(p.getInventory().getItemInMainHand().getType() != Material.STICK) {
+            return;
+        }
+
+       Block b = e.getBlock();
+        if (b.getState() instanceof Sign) {
+            Location loc = b.getLocation();
+            int X = loc.getBlockX();
+            int Y = loc.getBlockY();
+            int Z = loc.getBlockZ();
+            Bukkit.getLogger().log(Level.WARNING, "Clicked block at " + X + "," + Y + "," + Z);
+
+            plugin.setCurrentBlock(b);
+            p.sendMessage(ChatHelper.format("Current block set!"));
+
+            e.setCancelled(true);
+        }
     }
 }
--- src/main/java/com/opennms/minecraftnoc/MinecraftNOC.java
+++ src/main/java/com/opennms/minecraftnoc/MinecraftNOC.java
@@ -1,18 +1,22 @@
 package com.opennms.minecraftnoc;
 
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MinecraftNOC extends JavaPlugin {
+    private Block currentBlock;
+    private Entity currentEntity;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
         getConfig().options().copyDefaults();
         saveDefaultConfig();
+
+        currentBlock = null;
+        currentEntity = null;
 
         PluginManager pm = Bukkit.getPluginManager();
         pm.registerEvents(new ClickListener(this), this);
@@ -25,4 +29,11 @@ public final class MinecraftNOC extends JavaPlugin {
         // Plugin shutdown logic
     }
 
+    public Block getCurrentBlock() { return currentBlock; }
+    public void setCurrentBlock(Block block) { currentBlock = block; }
+    public void clearCurrentBlock() { currentBlock = null; }
+
+    public Entity getCurrentEntity() { return currentEntity; }
+    public void setCurrentEntity(Entity ent) { currentEntity = ent; }
+    public void clearCurrentEntity() { currentEntity = null; }
 }
